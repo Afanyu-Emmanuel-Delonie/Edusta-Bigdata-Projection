28b0be442541e48fedff62dd806cfd80c1bdfcd6
unknown <migishavalens>
2025-11-28 09:38:58 +0200

    Fix ALLOWED_HOSTS to allow local development and Render deployment

--- edustat/settings.py
+++ edustat/settings.py
@@ -135,6 +135,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/'
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['*']
+
 
 
